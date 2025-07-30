995d7f548f35b035940c80b87e65789351b13449
raja-0464 <balusupatilakshminarmadha@gmail.com>
2025-07-30 14:17:07 +0530

    pushing new python file

---
+++ activity.py
@@ -0,0 +1,12 @@
+name="MLWEC"
+name=10.0
+ison=True
+letter="H"
+# Arithemetic- + -* / %
+#logical - and,or,not
+#comparison- <,>,==,!=,<=,>=
+#assignment-=,+=,-=,*=,/=
+if (numder>5):
+    print(name)
+else:
+    print(ison)    
